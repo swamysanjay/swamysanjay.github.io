87506fe7e9fc2ca02935e13e680d85de1d0e6f14
swamysanjay <swamysanjay98@gmail.com>
2021-01-10 10:03:06 -0500

    Updates

---
+++ precache-manifest.cb24ea2c233c81978591eb1313b5a07a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d46883f37db63326a07a64f0ec72e2",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "4ab020ed50b15fdcf1ba",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/home/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ab020ed50b15fdcf1ba",
+    "url": "/home/static/js/main.26588a94.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "be888117c1392c3b4dc6f92c781ea29a",
+    "url": "/home/static/media/resume.be888117.pdf"
+  },
+  {
+    "revision": "476c5239b6955c4656ae40f9ae908fed",
+    "url": "/home/static/media/sanjayswamy.476c5239.JPG"
+  }
+]);
