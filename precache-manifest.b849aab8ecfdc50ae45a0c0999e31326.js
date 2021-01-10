6fadb7bcc5beaa8ceec23a673dd4b218ad3ec4f5
swamysanjay <swamysanjay98@gmail.com>
2021-01-10 14:32:03 -0500

    Updates

---
+++ precache-manifest.b849aab8ecfdc50ae45a0c0999e31326.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228f045b8325628cd0561024153d9457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bec29bae0204a7e7ee8f",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bec29bae0204a7e7ee8f",
+    "url": "/static/js/main.6030a347.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "be888117c1392c3b4dc6f92c781ea29a",
+    "url": "/static/media/resume.be888117.pdf"
+  },
+  {
+    "revision": "b169691d869ac813eed147a9ff341e18",
+    "url": "/static/media/sanjayswamy.b169691d.png"
+  }
+]);
